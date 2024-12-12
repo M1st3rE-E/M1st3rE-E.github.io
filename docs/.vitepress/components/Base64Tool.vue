0149aa354c99607bb28200f85a47b709d34980e3
RetherSzu <rether.szu@gmail.com>
2024-12-12 22:51:09 +0100

    :sparkles: Add new tools (base64 encode/decode)

---
+++ docs/.vitepress/components/Base64Tool.vue
@@ -0,0 +1,95 @@
+<template>
+    <textarea
+        v-model="input"
+        placeholder="Enter text here"
+        rows="5"
+    />
+    <div class="buttons">
+        <button @click="encode">Encode</button>
+        <button @click="decode">Decode</button>
+        <button @click="clear">Clear</button>
+    </div>
+    <textarea
+        v-model="output"
+        placeholder="Output will appear here"
+        rows="5"
+        readonly
+    />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            input: "",
+            output: "",
+        };
+    },
+    methods: {
+        encode() {
+            this.output = btoa(this.input);
+        },
+        decode() {
+            try {
+                this.output = atob(this.input);
+            } catch (e) {
+                this.output = "Error: Invalid Base64 string.";
+            }
+        },
+        clear() {
+            this.input = "";
+            this.output = "";
+        },
+    },
+};
+</script>
+
+<style scoped>
+
+textarea {
+    width: 100%;
+    font-family: inherit;
+    margin: 0.5em 0;
+    background: var(--vp-sidebar-bg-color);
+    color: var(--vp-c-text-1);
+    border-radius: 1rem;
+    padding: 1rem;
+}
+
+textarea[readonly] {
+    background: var(--vp-sidebar-bg-color);
+    color: var(--vp-c-text-2)
+}
+
+label {
+    font-size: 0.9em;
+    display: inline-flex;
+    align-items: center;
+}
+
+input[type="checkbox"] {
+    margin-right: 0.5em;
+}
+
+.buttons {
+    margin: 0.5em 0;
+}
+
+button {
+    margin-right: 0.5em;
+    padding: .25rem .5rem;
+    border-radius: 8px;
+    color: var(--vp-c-text-1);
+    cursor: pointer;
+    border: 1px solid var(--vp-c-divider);
+    transition: border-color 0.25s;
+}
+
+button:hover {
+    border-color: var(--vp-c-brand-1);
+}
+
+input[type="checkbox"]:checked {
+    accent-color: var(--vp-c-brand-1);
+}
+</style>
---
+++ docs/src/tools/base64-encoding.md
@@ -0,0 +1,22 @@
+---
+title: Base64 Encoder/Decoder Tool
+---
+
+[//]: # (@formatter:off)
+<script setup>
+import Base64Tool from "../../.vitepress/components/Base64Tool.vue";
+</script>
+[//]: # (@formatter:on)
+
+# Base64 Encoder/Decoder Tool
+
+<Base64Tool />
+
+## What is Base64 Encoding?
+
+Base64 encoding is a way to encode binary data into a format that can be transmitted over the internet. It is used to
+convert binary data into a text format that is safe to transmit over text-based protocols, such as email or HTTP.
+
+## References
+
+- [Base64 Encoding](https://en.wikipedia.org/wiki/Base64)
